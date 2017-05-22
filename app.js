47756f8204d80df7a596b607554d5b5f18fd7797
Jeffrey Axel Aguirre Valera <jeffreyaguirre@MacBook-Pro-de-Jeffrey.local>
2017-05-22 15:50:56 -0500

    update

--- app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-// ejm:
-var mongo = require('./helpers/mongo');
-var db = new mongo.DB();
-
-db.connect()
--- helpers/mongo.d.ts
+++
@@ -1,8 +0,0 @@
-import { Collection } from 'mongodb';
-export declare class DB {
-    private _db;
-    private _persons;
-    readonly persons: Collection;
-    connect(): Promise<void>;
-    close(): void;
-}
